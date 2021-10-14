30ced2d32f502c3cba5945ac9b2fff0d66cc5b9d
AASHIRWAD KUMAR <47081802+aashirwad01@users.noreply.github.com>
2021-10-14 20:39:51 +0530

    just some random stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./styles.css";
 
+const message = " i am cool";
+
 export default function App() {
+  //functionality goes here 
+
+
+  // template goes here
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>{message}</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
